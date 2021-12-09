a4b53584c1010c5301d369c73e297b3c4221485d
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-12-09 08:35:43 +0100

    opened more routes linked to user, outside of devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :views
+
   root to: "home#index"
-  devise_for :users
+  devise_for :users, :path_prefix => 'd'
+  resources :users, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
