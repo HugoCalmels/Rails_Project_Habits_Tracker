49528aa5fbaf1e1556e7ca8fef3839fe9c989a95
Hugo Calmels <calmels.hugo@yahoo.fr>
2021-12-14 15:20:25 +0100

    getting start on checkpoints mvp

--- app/controllers/good_habits_controller.rb
+++ app/controllers/good_habits_controller.rb
@@ -1,21 +1,10 @@
 class GoodHabitsController < ApplicationController
   before_action :authenticate_user!
+
   def index
-    puts params[:user_id]
-    puts params[:cycle_title]
     @cycle = Cycle.find_by(title: params[:cycle_title])
     @user = User.find(params[:user_id])
-
-    GoodHabit.create(
-      title: "from the controller",
-      checkpoints: 1,
-      user: @user,
-      cycle: @cycle
-    )
-    puts "%%%%%%%%%%%%"
-
     @good_habits = GoodHabit.where(cycle_id: @cycle.id, user_id: @user.id)
-
   end
 
   def show
@@ -30,9 +19,6 @@ class GoodHabitsController < ApplicationController
   def create
     @cycle = Cycle.find_by(title: params[:cycle_title])
     @user = User.find(params[:user_id])
-    puts "%%%%%%%%%%%%%%%"
-    puts params
-    puts "%%%%%%%%%%%%%%%"
     @good_habit  = GoodHabit.new(good_habit_params)
     @good_habit.user = @user
     @good_habit.cycle = @cycle
@@ -52,15 +38,11 @@ class GoodHabitsController < ApplicationController
   def destroy
   end
 
-
-
   private
-  
 
-    # Only allow a list of trusted parameters through.
-    def good_habit_params
-      params.require(:good_habit).permit(:title, :checkpoints)
-    end
+  def good_habit_params
+    params.require(:good_habit).permit(:title, :checkpoints)
+  end
 
 end
 
